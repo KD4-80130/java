e5fae8f08426051f053622853226a075ed65f830
naveenp7461 <nrpme107@gmail.com>
2023-10-10 16:14:40 +0530

    uploaede

---
+++ ASSIGN-3/Question_2/src/com/app/tester/Tester.java
@@ -0,0 +1,14 @@
+package com.app.tester;
+import com.app.geometry.Point;
+public class Tester {
+
+	public static void main(String[] args) {
+//		Point p1= new Point();
+//		Point p2 =new Point(1,2);
+		
+	   // p1.calculteDistance(p2);
+		TestpointArray1 T =new TestpointArray1();
+		T.countPoints();
+		
+	}
+}
